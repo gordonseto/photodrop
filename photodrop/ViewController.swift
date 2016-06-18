d1a68171efc65bc85c83fc99d99d119c7e6ce3e5
gordonseto <gordon.seto@gmail.com>
2016-06-18 13:11:18 -0600

    added map and user location zoom-in

--- photodrop/ViewController.swift
+++ photodrop/ViewController.swift
@@ -7,19 +7,33 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(animated: Bool) {
+        locationManager.requestWhenInUseAuthorization()
+        self.mapView.userLocation.addObserver(self, forKeyPath: "location", options: NSKeyValueObservingOptions(), context: nil)
     }
-
+    
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        
+        if self.mapView.showsUserLocation && self.mapView.userLocation.location != nil {
+            
+            let span = MKCoordinateSpanMake(0.0125, 0.0125)
+            let region = MKCoordinateRegion(center: self.mapView.userLocation.location!.coordinate, span: span)
+            self.mapView.setRegion(region, animated: true)
+        }
+    }
 
 }
 
